3c7d1c70938d0bb7b55ea8898b2fafee6fa9baf9
mizukiarashiba <mizuki.arashiba@rakuten.com>
2018-06-18 04:08:34 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,18 +4,27 @@
 
 <h1>タスク新規作成ページ</h1>
 
+
+<div class="row">
+    <div class="col-xs-6">
     
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
         
+        <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
         {!! Form::text('status') !!}
-
-
+        </div>
+    
+        <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
-
+        </div>
+    
         {!! Form::submit('投稿') !!}
 
     {!! Form::close() !!}
+    
+    </div>
+</div>
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,18 +4,26 @@
 
 <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
-
+<div class="row">
+    <div class="col-xs-6">
 
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-
+        
+        <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
         {!! Form::text('staus') !!}
+        </div>
         
+        <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
-
+        </div>
+        
         {!! Form::submit('更新') !!}
 
     {!! Form::close() !!}
+    
+    </div>
+</div>
 
 @endsection
